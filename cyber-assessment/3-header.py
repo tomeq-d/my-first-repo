4d0cd76d25829c40c582d26fb2a18c30ff567067
tomeq-d <tdembowski@gmail.com>
2026-03-29 20:42:07 +0000

    Add task 4: fetch X-Request-Id header using urllib

---
+++ cyber-assessment/3-header.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.getheader("X-Request-Id"))
